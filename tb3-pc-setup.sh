dfeebaef9037bafc96b90b1bca4fe1f89fa4385f
Mr. Piya Pimchankam <piya.pimchankam@gmail.com>
2020-07-05 14:21:13 +0700

    Add files via upload

--- tb3-pc-setup.sh
+++ tb3-pc-setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install ros-melodic-joy ros-melodic-teleop-twist-joy ros-melodic-teleop-twist-keyboard ros-melodic-laser-proc ros-melodic-rgbd-launch ros-melodic-depthimage-to-laserscan ros-melodic-rosserial-arduino ros-melodic-rosserial-python ros-melodic-rosserial-server ros-melodic-rosserial-client ros-melodic-rosserial-msgs ros-melodic-amcl ros-melodic-map-server ros-melodic-move-base ros-melodic-urdf ros-melodic-xacro ros-melodic-compressed-image-transport ros-melodic-rqt-image-view ros-melodic-gmapping ros-melodic-navigation ros-melodic-interactive-markers
+sudo apt-get install -y ros-melodic-joy ros-melodic-teleop-twist-joy ros-melodic-teleop-twist-keyboard ros-melodic-laser-proc ros-melodic-rgbd-launch ros-melodic-depthimage-to-laserscan ros-melodic-rosserial-arduino ros-melodic-rosserial-python ros-melodic-rosserial-server ros-melodic-rosserial-client ros-melodic-rosserial-msgs ros-melodic-amcl ros-melodic-map-server ros-melodic-move-base ros-melodic-urdf ros-melodic-xacro ros-melodic-compressed-image-transport ros-melodic-rqt-image-view ros-melodic-gmapping ros-melodic-navigation ros-melodic-interactive-markers
+
+sudo apt-get install -y ros-melodic-tf2-ros
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
@@ -9,6 +11,7 @@ git clone -b melodic-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs
 git clone -b melodic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations
 git clone -b melodic-devel https://github.com/ROBOTIS-GIT/turtlebot3
 git clone -b melodic-devel https://github.com/ROBOTIS-GIT/hls_lfcd_lds_driver
+git clone https://github.com/RobotWebTools/tf2_web_republisher.git
 
 cd ~/catkin_ws/
 rosdep update
